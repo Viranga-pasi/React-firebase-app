a651666703c45e745468ff6b9fdef523e0ab830b
Viranga Jayawardana <virangapasdindu4@gmail.com>
2020-08-19 13:17:02 +0530

    correct finals

--- src/Components/Quiz/FinalScore.js
+++ src/Components/Quiz/FinalScore.js
@@ -3,37 +3,12 @@ import firebase from "../../firebase";
 
 const FinalScore = (props) => {
   // console.log(props.userDetails.email[props.userIndex]);
-  let cue = props.userDetails.email[props.userIndex];
+
   const userScore = {
     attempt: props.attempt.map((score) => score.attemptId),
     score: props.attempt.map((score) => score.attemptScore),
   };
-  const [userId, setUserId] = useState([]);
-  // console.log(userScore);
-  useEffect(() => {
-    const fetchData = async () => {
-      const db = firebase.firestore();
-      const data = await db.collection("user_details").get();
 
-      setUserId(data.docs.map((doc) => doc.id)[props.userIndex]);
-    };
-    fetchData();
-  }, []);
-
-  // console.log("current user id", userId);
-
-  var cUser = firebase.auth().currentUser;
-  const updateData = async () => {
-    const db = firebase.firestore();
-    let usersRef = await db.collection("user_details");
-    let { docs } = await usersRef.where("uid", "==", cUser.uid).limit(1).get();
-    const { attempts } = docs[0].data();
-    await db.collection("user_details").doc(docs[0].id).update({
-      attempts: userScore.score,
-    });
-  };
-
-  updateData();
   return (
     <div>
       <ul>
